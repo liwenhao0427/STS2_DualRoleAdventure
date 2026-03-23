6780478150f1bab061c440414827bac4f7eb7ef8
liwenhao0427 <liwenhao0427@163.com>
2026-03-23 19:41:08 +0800

    fix: 补齐永久低语耳环的+1能量效果

--- Scripts/Models/Relics/LocalWakuuStarterRelic.cs
+++ Scripts/Models/Relics/LocalWakuuStarterRelic.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using LocalMultiControl.Scripts.Runtime;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 
 namespace LocalMultiControl.Scripts.Models.Relics;
@@ -12,6 +15,22 @@ internal sealed class LocalWakuuStarterRelic : RelicModel
     public override RelicRarity Rarity => RelicRarity.Event;
 
     protected override string IconBaseName => "whispering_earring";
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        new[] { HoverTipFactory.ForEnergy(this) };
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        new DynamicVar[] { new EnergyVar(1) };
+
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        if (player != Owner)
+        {
+            return amount;
+        }
+
+        return amount + DynamicVars.Energy.BaseValue;
+    }
 
     public override Task BeforePlayPhaseStart(PlayerChoiceContext choiceContext, Player player)
     {
