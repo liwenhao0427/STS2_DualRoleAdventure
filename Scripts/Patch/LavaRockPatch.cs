75a556d20cbfaaabde4b9b9e8c48e231936cd04c
liwenhao0427 <liwenhao0427@163.com>
2026-04-13 16:43:15 +0800

    fix: 修正熔岩石额外首领奖励归属

--- Scripts/Patch/LavaRockPatch.cs
+++ Scripts/Patch/LavaRockPatch.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using HarmonyLib;
 using LocalMultiControl.Scripts.Runtime;
 using MegaCrit.Sts2.Core.Entities.Players;
@@ -44,34 +43,26 @@ internal static class LavaRockPatch
             return false;
         }
 
-        List<LavaRock> pendingLavaRocks = player.RunState.Players
-            .Select((member) => member.GetRelic<LavaRock>())
-            .Where((relic) => relic != null && !relic.HasTriggered)
-            .Cast<LavaRock>()
-            .ToList();
-        if (pendingLavaRocks.Count == 0)
+        LavaRock? lavaRock = player.GetRelic<LavaRock>();
+        if (lavaRock == null || lavaRock.HasTriggered)
         {
             __result = false;
             return false;
         }
 
-        int extraRelicCount = pendingLavaRocks[0].DynamicVars["Relics"].IntValue;
+        int extraRelicCount = lavaRock.DynamicVars["Relics"].IntValue;
         for (int i = 0; i < extraRelicCount; i++)
         {
             rewards.Add(new RelicReward(player));
         }
 
-        foreach (LavaRock lavaRock in pendingLavaRocks)
-        {
-            lavaRock.Flash();
-            lavaRock.HasTriggered = true;
-            lavaRock.Status = RelicStatus.Disabled;
-        }
+        lavaRock.Flash();
+        lavaRock.HasTriggered = true;
+        lavaRock.Status = RelicStatus.Disabled;
 
         __result = true;
-        string owners = string.Join(",", pendingLavaRocks.Select((relic) => relic.Owner.NetId));
         LocalMultiControlLogger.Info(
-            $"本地多控已按队伍熔岩石触发首领额外遗物: rewardPlayer={player.NetId}, owners={owners}, added={extraRelicCount}");
+            $"本地多控已按角色熔岩石触发首领额外遗物: rewardPlayer={player.NetId}, added={extraRelicCount}");
         return false;
     }
 }
